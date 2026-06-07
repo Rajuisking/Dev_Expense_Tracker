852d63f3b8263b97a2ceb83be79f1a28c5a9cc20
Rajuisking <rajeshsahani3455@gmail.com>
2026-06-07 11:47:49 +0530

    connect frontend to render backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://dev-expense-tracker-ves6.onrender.com/api",
-  headers: { "Content-Type": "application/json" },
+  baseURL: import.meta.env.VITE_API_URL || "/api"
 });
 
 export default API;
